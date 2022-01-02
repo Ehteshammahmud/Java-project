3aa93a3360e473f71237b20cc68f0acd0495f205
Ehteshammahmud <mmsamsdu@gmail.com>
2022-01-02 14:50:19 +0100

    Mortgage Calculation

--- src/com/codewithmahmud/Main.java
+++ src/com/codewithmahmud/Main.java
@@ -1,8 +1,23 @@
 package com.codewithmahmud;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello Java Developers");
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        Scanner scanner = new Scanner(System.in);
+
+        int principal = 0;
+        while (true) {
+            System.out.println("Principal: ");
+            principal= scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1000 and 1000000");
+        }
+
+
     }
 }
